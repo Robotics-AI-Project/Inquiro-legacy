2690d404b7903ae8f93437ec781648715ae311f3
tontan2545 <tontan2545@gmail.com>
2023-10-30 15:17:04 +0700

    feat: added few echos

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,11 @@
 ROOT_DIR=$(pwd)
 
+echo "========================================"
+echo "🚌 Installing Poetry dependencies"
+
 cd $ROOT_DIR/api && poetry install && poetry run task codegen
+
+echo "========================================"
+echo "🚌 Installing Next.js dependencies"
 
 cd $ROOT_DIR/web && bun install
