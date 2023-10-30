9a4529c0cb4350d2d1684890bed37bbae57547b0
tontan2545 <tontan2545@gmail.com>
2023-10-30 15:19:45 +0700

    feat: WIP modes

--- api/app/utils/sql_generation/din_sql/module/schema_linking.py
+++ api/app/utils/sql_generation/din_sql/module/schema_linking.py
@@ -1,95 +1,8 @@
-from app.utils.sql_generation.din_sql.utils.helper import (
-    get_database_foreign_keys,
+from app.utils.sql_generation.din_sql.utils import (
     get_database_fields,
+    get_database_foreign_keys,
+    schema_linking_few_shot_prompt,
 )
-
-_few_shot_prompt = """Table advisor, columns = [*,s_ID,i_ID]
-Table classroom, columns = [*,building,room_number,capacity]
-Table course, columns = [*,course_id,title,dept_name,credits]
-Table department, columns = [*,dept_name,building,budget]
-Table instructor, columns = [*,ID,name,dept_name,salary]
-Table prereq, columns = [*,course_id,prereq_id]
-Table section, columns = [*,course_id,sec_id,semester,year,building,room_number,time_slot_id]
-Table student, columns = [*,ID,name,dept_name,tot_cred]
-Table takes, columns = [*,ID,course_id,sec_id,semester,year,grade]
-Table teaches, columns = [*,ID,course_id,sec_id,semester,year]
-Table time_slot, columns = [*,time_slot_id,day,start_hr,start_min,end_hr,end_min]
-Foreign_keys = [course.dept_name = department.dept_name,instructor.dept_name = department.dept_name,section.building = classroom.building,section.room_number = classroom.room_number,section.course_id = course.course_id,teaches.ID = instructor.ID,teaches.course_id = section.course_id,teaches.sec_id = section.sec_id,teaches.semester = section.semester,teaches.year = section.year,student.dept_name = department.dept_name,takes.ID = student.ID,takes.course_id = section.course_id,takes.sec_id = section.sec_id,takes.semester = section.semester,takes.year = section.year,advisor.s_ID = student.ID,advisor.i_ID = instructor.ID,prereq.prereq_id = course.course_id,prereq.course_id = course.course_id]
-Q: "Find the buildings which have rooms with capacity more than 50."
-A: Let’s think step by step. In the question "Find the buildings which have rooms with capacity more than 50.", we are asked:
-"the buildings which have rooms" so we need column = [classroom.capacity]
-"rooms with capacity" so we need column = [classroom.building]
-Based on the columns and tables, we need these Foreign_keys = [].
-Based on the tables, columns, and Foreign_keys, The set of possible cell values are = [50]. So the Schema_links are:
-Schema_links: [classroom.building,classroom.capacity,50]
-
-Table department, columns = [*,Department_ID,Name,Creation,Ranking,Budget_in_Billions,Num_Employees]
-Table head, columns = [*,head_ID,name,born_state,age]
-Table management, columns = [*,department_ID,head_ID,temporary_acting]
-Foreign_keys = [management.head_ID = head.head_ID,management.department_ID = department.Department_ID]
-Q: "How many heads of the departments are older than 56 ?"
-A: Let’s think step by step. In the question "How many heads of the departments are older than 56 ?", we are asked:
-"How many heads of the departments" so we need column = [head.*]
-"older" so we need column = [head.age]
-Based on the columns and tables, we need these Foreign_keys = [].
-Based on the tables, columns, and Foreign_keys, The set of possible cell values are = [56]. So the Schema_links are:
-Schema_links: [head.*,head.age,56]
-
-Table department, columns = [*,Department_ID,Name,Creation,Ranking,Budget_in_Billions,Num_Employees]
-Table head, columns = [*,head_ID,name,born_state,age]
-Table management, columns = [*,department_ID,head_ID,temporary_acting]
-Foreign_keys = [management.head_ID = head.head_ID,management.department_ID = department.Department_ID]
-Q: "what are the distinct creation years of the departments managed by a secretary born in state 'Alabama'?"
-A: Let’s think step by step. In the question "what are the distinct creation years of the departments managed by a secretary born in state 'Alabama'?", we are asked:
-"distinct creation years of the departments" so we need column = [department.Creation]
-"departments managed by" so we need column = [management.department_ID]
-"born in" so we need column = [head.born_state]
-Based on the columns and tables, we need these Foreign_keys = [department.Department_ID = management.department_ID,management.head_ID = head.head_ID].
-Based on the tables, columns, and Foreign_keys, The set of possible cell values are = ['Alabama']. So the Schema_links are:
-Schema_links: [department.Creation,department.Department_ID = management.department_ID,head.head_ID = management.head_ID,head.born_state,'Alabama']
-
-Table advisor, columns = [*,s_ID,i_ID]
-Table classroom, columns = [*,building,room_number,capacity]
-Table course, columns = [*,course_id,title,dept_name,credits]
-Table department, columns = [*,dept_name,building,budget]
-Table instructor, columns = [*,ID,name,dept_name,salary]
-Table prereq, columns = [*,course_id,prereq_id]
-Table section, columns = [*,course_id,sec_id,semester,year,building,room_number,time_slot_id]
-Table student, columns = [*,ID,name,dept_name,tot_cred]
-Table takes, columns = [*,ID,course_id,sec_id,semester,year,grade]
-Table teaches, columns = [*,ID,course_id,sec_id,semester,year]
-Table time_slot, columns = [*,time_slot_id,day,start_hr,start_min,end_hr,end_min]
-Foreign_keys = [course.dept_name = department.dept_name,instructor.dept_name = department.dept_name,section.building = classroom.building,section.room_number = classroom.room_number,section.course_id = course.course_id,teaches.ID = instructor.ID,teaches.course_id = section.course_id,teaches.sec_id = section.sec_id,teaches.semester = section.semester,teaches.year = section.year,student.dept_name = department.dept_name,takes.ID = student.ID,takes.course_id = section.course_id,takes.sec_id = section.sec_id,takes.semester = section.semester,takes.year = section.year,advisor.s_ID = student.ID,advisor.i_ID = instructor.ID,prereq.prereq_id = course.course_id,prereq.course_id = course.course_id]
-Q: "Give the title of the course offered in Chandler during the Fall of 2010."
-A: Let’s think step by step. In the question "Give the title of the course offered in Chandler during the Fall of 2010.", we are asked:
-"title of the course" so we need column = [course.title]
-"course offered in Chandler" so we need column = [SECTION.building]
-"during the Fall" so we need column = [SECTION.semester]
-"of 2010" so we need column = [SECTION.year]
-Based on the columns and tables, we need these Foreign_keys = [course.course_id = SECTION.course_id].
-Based on the tables, columns, and Foreign_keys, The set of possible cell values are = [Chandler,Fall,2010]. So the Schema_links are:
-Schema_links: [course.title,course.course_id = SECTION.course_id,SECTION.building,SECTION.year,SECTION.semester,Chandler,Fall,2010]
-
-Table advisor, columns = [*,s_ID,i_ID]
-Table classroom, columns = [*,building,room_number,capacity]
-Table course, columns = [*,course_id,title,dept_name,credits]
-Table department, columns = [*,dept_name,building,budget]
-Table instructor, columns = [*,ID,name,dept_name,salary]
-Table prereq, columns = [*,course_id,prereq_id]
-Table section, columns = [*,course_id,sec_id,semester,year,building,room_number,time_slot_id]
-Table student, columns = [*,ID,name,dept_name,tot_cred]
-Table takes, columns = [*,ID,course_id,sec_id,semester,year,grade]
-Table teaches, columns = [*,ID,course_id,sec_id,semester,year]
-Table time_slot, columns = [*,time_slot_id,day,start_hr,start_min,end_hr,end_min]
-Foreign_keys = [course.dept_name = department.dept_name,instructor.dept_name = department.dept_name,section.building = classroom.building,section.room_number = classroom.room_number,section.course_id = course.course_id,teaches.ID = instructor.ID,teaches.course_id = section.course_id,teaches.sec_id = section.sec_id,teaches.semester = section.semester,teaches.year = section.year,student.dept_name = department.dept_name,takes.ID = student.ID,takes.course_id = section.course_id,takes.sec_id = section.sec_id,takes.semester = section.semester,takes.year = section.year,advisor.s_ID = student.ID,advisor.i_ID = instructor.ID,prereq.prereq_id = course.course_id,prereq.course_id = course.course_id]
-Q: "Find the id of instructors who taught a class in Fall 2009 but not in Spring 2010."
-A: Let’s think step by step. In the question "Find the id of instructors who taught a class in Fall 2009 but not in Spring 2010.", we are asked:
-"id of instructors who taught " so we need column = [teaches.id]
-"taught a class in" so we need column = [teaches.semester,teaches.year]
-Based on the columns and tables, we need these Foreign_keys = [].
-Based on the tables, columns, and Foreign_keys, The set of possible cell values are = [Fall,2009,Spring,2010]. So the Schema_links are:
-Schema_links: [teaches.id,teaches.semester,teaches.year,Fall,2009,Spring,2010]
-"""
 
 
 def get_schema_links(question, database):
@@ -104,10 +17,5 @@ def schema_linking_prompt_maker(question, database):
     instruction = "# Find the schema_links for generating SQL queries for each question based on the database schemas and Foreign keys."
     fields = get_database_fields(database)
     foreign_keys = get_database_foreign_keys(database)
-    prompt = f"""{instruction}
-{_few_shot_prompt}
-{fields}
-{foreign_keys}
-Q: "{question}"
-A: Let’s think step by step."""
+    prompt = f"""{instruction}\n{schema_linking_few_shot_prompt}\n{fields}\n{foreign_keys}\nQ: "{question}"\nA: Let’s think step by step."""
     return prompt
--- api/app/utils/sql_generation/din_sql/module/sql_generation.py
+++ api/app/utils/sql_generation/din_sql/module/sql_generation.py
@@ -1,2 +1,24 @@
-# def generate_sql(self, prompt: str, *args, **kwargs) -> str:
-#     return output["output"]["choices"][0]["text"]
+from app.utils.llm import model
+from app.utils.sql_generation.din_sql.utils.types import SqlClass
+
+
+def get_sql(prompt: str, sql_class: SqlClass) -> str:
+    class_prompt = __get_prompt_from_class(prompt, sql_class)
+    sql = model.generate(class_prompt, stop=["Q:", "\\n\\n"])
+    return sql
+
+
+def __get_prompt_from_class(prompt: str, sql_class: SqlClass) -> str:
+    match sql_class:
+        case "EASY":
+            # TODO
+            pass
+        case "NESTED":
+            # TODO
+            pass
+        case "NON-NESTED":
+            # TODO
+            pass
+        case _:
+            raise ValueError(f"Invalid sql_class: {sql_class}")
+    return "SELECT COUNT(*) FROM SINGER"
