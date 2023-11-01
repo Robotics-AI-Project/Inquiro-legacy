8e227f54224f7682b8b96da2d4c67f2197dfd06e
tontan2545 <tontan2545@gmail.com>
2023-11-01 16:52:35 +0700

    feat: change default to C3-SQL with GPT3.5

--- api/app/modules/inference/sql/dto.py
+++ api/app/modules/inference/sql/dto.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel, Field
 class GenerateSQLDTO(BaseModel):
     prompt: str
     db_url: str = Field(alias="dbUrl")
-    generator: str = Field(default="DIN-SQL")
-    model: str = Field(default="GPT-4")
+    generator: str = Field(default="C3-SQL")
+    model: str = Field(default="GPT-3.5-turbo")
